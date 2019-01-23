26d795c4b42cd4b102b8562fc2987787e384c196
Kevin Middlesworth <kmiddlesworth@gmail.com>
2019-01-22 19:31:46 -0600

    updating vue-socket

--- admin/app.js
+++ admin/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
-import socketio from 'socket.io-client';
+//import socketio from 'socket.io-client';
 import vueSocket from 'vue-socket.io';
 
 Vue.config.devtools = true;
@@ -9,11 +9,20 @@ import App from './components/App/App.vue';
 import router from './router';
 import store from './store';
 
-const socketInstance = socketio(window.location.origin);
-
-Vue.use(vueSocket, socketInstance, store);
+//const socketInstance = socketio(window.location.origin);
 
 sync(store, router);
+
+//Vue.use(vueSocket, socketInstance, store);
+Vue.use(new vueSocket({
+    debug: true,
+    connection: window.location.origin,
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    }
+}));
 
 const app = new Vue({
 	router,
